a9977b9e00c0c55c16fa517238f1a9523cb68035
wangyuan <2814579279@qq.com>
2018-10-30 10:47:31 +0800

    图表组件开发，增加参数输入

--- src/app/routes/developer/charts/echarts-dev.service.ts
+++ src/app/routes/developer/charts/echarts-dev.service.ts
@@ -1,25 +1,30 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from "rxjs/Observable";
+import {Observable} from 'rxjs/Observable';
 
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/of';
 
 
 @Injectable()
 export class EchartsDevService {
-    private SERVERAPI = 'http://localhost:8080';
+    private SERVERAPI = 'http://localhost:3000';
 
     constructor(private http: HttpClient) {
     }
 
     qryAllServiceList() {
-        return Observable.from([]);
-        // return this.http.post(this.SERVERAPI + 'visible/chartsDevelop/chartModel/qryAllServiceList', {})
-        //     .map((data: any) => data.retList);
+        // return Observable.of([]);
+        return this.http.get(this.SERVERAPI + '/serviceList', {});
+        // .map((data: any) => {
+        //     console.log(data);
+        //     return data;
+        // });
     }
 
     preview(params: any) {
         params.chartType = '0';
+        console.log({...params})
         return this.http.post(this.SERVERAPI + 'visible/chartsDevelop/chartModel/preview', {...params})
             .map(data => data['retData']);
     }
